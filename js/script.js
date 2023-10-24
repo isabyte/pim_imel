654387f60372963a6b684ce0cf422055ada680c4
isabyte <isabelamigli@gmail.com>
2023-10-23 21:20:00 -0300

    separação do js em outro arquivo

--- js/script.js
+++ js/script.js
@@ -1,21 +1,144 @@
+/* CONTATO DASHBOARD */
 
+function verificaInputsContato() {
+  var tel = document.getElementById('telContatoDash').value;
+  var email = document.getElementById('emailContatoDash').value;
+  var endereco = document.getElementById('enderecoContatoDash').value;
 
-
-function cancelar() {
-    if(confirm('Tem certeza que deseja cancelar? Todos os dados serão perdidos') == true) {
-      window.location.reload();
-    }
+  if(tel.length < 16 || email.length < 3 || endereco.length < 10) {
+    alert('Preencha todos os campos corretamente');
+    return false;
   }
+  return true;
+}
 
-  function salvar() {
-    if(!verificaInputs()) {
+function editarContato() {
+  // desabilita o botão de editar
+  document.getElementById('botao-editar').disabled = true;
+
+  // pega o input invisível do telefone
+  var tel = document.getElementById('telContatoDash');
+
+  // pega o parágrafo do telefone
+  var pTel = document.getElementById('p-telContatoDash');
+
+  // torna o input do telefone visível
+  tel.style.display = "block";
+
+  // torna o parágrafo do telefone invisível
+  pTel.style.display = "none";
+
+  /* o código abaixo segue o mesmo padrão do acima */
+
+  var email = document.getElementById('emailContatoDash');
+  var pEmail = document.getElementById('p-emailContatoDash');
+
+  email.style.display = "block";
+  pEmail.style.display = "none";
+
+  var endereco = document.getElementById('enderecoContatoDash');
+  var pEndereco = document.getElementById('p-enderecoContatoDash');
+
+  endereco.style.display = "block";
+  pEndereco.style.display = "none";
+
+  // pega a div onde os botões estão escondidos
+  var botoes = document.getElementById('div-botoes-salvar');
+
+  // torna os botões visíveis
+  botoes.style.display = "block";
+}
+
+function cancelarContato() {
+  // pega o input do telefone
+  var tel = document.getElementById('telContatoDash');
+
+  // pega o parágrafo do telefone
+  var pTel = document.getElementById('p-telContatoDash');
+
+  /* o código abaixo segue o mesmo padrão do acima */
+
+  var email = document.getElementById('emailContatoDash');
+  var pEmail = document.getElementById('p-emailContatoDash');
+
+  var endereco = document.getElementById('enderecoContatoDash');
+  var pEndereco = document.getElementById('p-enderecoContatoDash');
+
+  var salvar = document.getElementById('div-botoes-salvar');
+
+  if(confirm('Tem certeza que deseja cancelar? Todos os dados serão perdidos') == true) {
+  // restaura a página sem perder os dados antigos, diferentemente de location.reload() que recarrega a página
+
+      // habilita o botão de editar
+      document.getElementById('botao-editar').disabled = false;
+
+      // esconde os botaoes de salvar ou cancelar
+      salvar.style.display = "none";
+
+      // esconde os inputs
+      tel.style.display = "none";
+      email.style.display = "none";
+      endereco.style.display = "none";
+
+      // mostra os parágrafos
+      pTel.style.display = "block";
+      pEmail.style.display = "block";
+      pEndereco.style.display = "block";
+
+  }
+}
+
+function salvarContato() {
+  // pega o input do telefone
+  var tel = document.getElementById('telContatoDash');
+
+  // pega o parágrafo do telefone
+  var pTel = document.getElementById('p-telContatoDash');
+
+  /* o código abaixo segue o mesmo padrão do acima */
+
+  var email = document.getElementById('emailContatoDash');
+  var pEmail = document.getElementById('p-emailContatoDash');
+
+  var endereco = document.getElementById('enderecoContatoDash');
+  var pEndereco = document.getElementById('p-enderecoContatoDash');
+  
+  if(!verificaInputsContato()) {
       return;
-    }
-
-    if(confirm('Deseja salvar as alterações?') == true) {
-      window.location.reload(); 
-    }
   }
+
+  if(confirm('Deseja salvar as alterações?') == true) {
+      // coloca o valor digitado no input do telefone no parágrafo
+      pTel.innerText = tel.value;
+
+      // torna o input invisível e o parágrafo visível com o novo valor
+      tel.style.display = "none";
+      pTel.style.display = "block";
+
+      /* o código abaixo segue o mesmo padrão do acima */
+
+      pEmail.innerText = email.value;
+
+      email.style.display = "none";
+      pEmail.style.display = "block";
+
+      pEndereco.innerText = endereco.value;
+
+      endereco.style.display = "none";
+      pEndereco.style.display = "block";
+
+      // habilita o botão de editar
+      document.getElementById('botao-editar').disabled = false;
+
+      // pega a div onde os botões de salvar ou cancelar estão
+      var botoes = document.getElementById('div-botoes-salvar');
+
+      // esconde os botões
+      botoes.style.display = "none";
+  }
+}
+
+/* FIM CONTATO DASHBOARD */
 
 function editarJustificativa() {
     const texto = document.getElementById('texto');
